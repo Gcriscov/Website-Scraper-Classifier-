5c82b322a69331bc37521559037a65f49f2095cf
Corpaci Luiza <corpaci.luiza@yahoo.com>
2019-03-26 16:10:11 +0200

    Cleaned Scraper
    Deleted some comments

--- Scraper.py
+++ Scraper.py
@@ -14,14 +14,16 @@ import urllib3
 fname = 'TrainingOEMs.txt'
 urllib3.disable_warnings()
 
+# Open url and return page content as BeautifulSoup object
 def get_dom(url):
 	try:
 		connection = requests.get(url, verify=False, allow_redirects=True)
 		return connection
 	except Exception as e: 
-		print('Exception:' + e)
+		print(e)
 	return connection
 
+# Return links from url
 def get_links(url):
 	u = url
 	url = get_dom(url)
@@ -29,6 +31,7 @@ def get_links(url):
 	href_set = {tag.get('href') for tag in tags}
 	return resolve_links((href for href in href_set), u)
 
+# Guess root for relative links
 def guess_root(links):
     for link in links:
         if link.find('http'):
@@ -37,30 +40,14 @@ def guess_root(links):
             netloc = parsed_link.netloc
             return scheme + netloc
 
+# Transform relative link to absolute link
 def resolve_links(links, url):
 	root = str(url)	
 	for link in links:
-		#link = l.get('href')
 		link = urljoin(root, link)
-			#try:
-			#	m = re.search('href.*?"/(.*?)"', str(link))
-			#except:
-			#	m = ''
-			#	link = ''
-			#if m:
-			#	link = str(root) + str(m.group(1))
-		print(link)
+		print("Webpage Link: " + link)
 		yield link
-
-def visible(element):
-    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
-        return False
-    elif re.match('<!--.*-->', str(element.encode('utf-8'))):
-        return False
-    return True
- 
-
-	
+# Clean html content
 def striphtml(data):
 	data = re.sub('<[^<]+?>', ' ', data)
 	data = data.replace('\n', ' ')
@@ -78,45 +65,22 @@ def striphtml(data):
 	return str(p.sub('', data))
 	
 with open(fname) as f:
-	content = f.readlines();
-	for line in content:
-		open_page = ''.join(line);
-		for link in get_links(open_page):
-				with open('out.txt', 'a') as fileID:
-					try:
-						print('1')
-						page = BeautifulSoup(get_dom(link).content, 'lxml')
-						body = page
-						for script in body('script'):
-							script.decompose()
-						for script in body('style'):
-							script.decompose()	
-						for script in body('head'):
-							script.decompose()
-						text = striphtml(str(body.text))
-						print('aici')
-						print(text)
-						fileID.write(str(text.encode('ascii', 'ignore')))
-						#for string in body.stripped_strings:
-						#	print(repr(string)))
-						#	fileID.write(repr(string).encode('utf-8'))
-						#result = filter(visible, page)
-						#print(striphtml(str(page.get_text().encode('utf-8'))))
-						#print(striphtml(str(page.text)))
-						#fileID.write(striphtml(str(page.get_text().encode('utf-8'))))
-						#fileID.write(str(page.text.encode('utf-8')))
-						print('2')
-						# #print(list(result))
-						# #for res in list(result):
-						# #	print('2.1')
-						# #    print(res)
-						# #	element = striphtml(str(res))
-							# #element = res.get_text()
-							# print('3')
-							# print(element)
-							# print('4')
-							# fileID.write(str(element.encode('utf-8')))
-					except Exception as e: 
-						print(e)
+    content = f.readlines();
+    for line in content:
+        open_page = ''.join(line);
+        for link in get_links(open_page):
+            with open('out.txt', 'a') as fileID:
+                try:
+                    page = BeautifulSoup(get_dom(link).content, 'lxml')
+		
+                    for script in page('script'):
+                        script.decompose() # delete all script modules
+                    for script in page('style'):
+                        script.decompose() # delete all style modules
 			
-		print('***************************************************************************')
+                    text = striphtml(str(page.text))
+                    print('Content:\n' + str(text.encode('ascii', 'ignore')))
+                    fileID.write(str(text.encode('ascii', 'ignore')))
+                except Exception as e: 
+			print(e)
+        print('***************************************************************************')
